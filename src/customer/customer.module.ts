d6862a7ff52118f52704fe80b6c9874e62185911
francislagueu <lagson6@yahoo.fr>
2019-04-14 00:26:18 -0700

    updated customer module and enable cors

--- src/customer/customer.module.ts
+++ src/customer/customer.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { CustomerController } from './customer.controller';
 import { CustomerService } from './customer.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CustomerSchema } from './models/schema/customer.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Customer', schema: CustomerSchema }]),
+  ],
   controllers: [CustomerController],
-  providers: [CustomerService]
+  providers: [CustomerService],
 })
 export class CustomerModule {}
