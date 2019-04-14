b8cc0e2ce412dfdcf1dd7daa51dfe126b63e39e6
francislagueu <lagson6@yahoo.fr>
2019-04-14 00:59:51 -0700

    generated user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CustomerModule } from './customer/customer.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { CustomerModule } from './customer/customer.module';
       useNewUrlParser: true,
     }),
     CustomerModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
